252d0e4f472a5c620bdb2b6bb2aa04e921e05235
camillez325 <czhang325@gmail.com>
2018-10-11 18:14:21 -0700

    first

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+	public String username;
+	public int postid;
+	public String title;
+	public String body;
+	public Post(String username, int postid, String title, String body) {
+		this.username = username;
+		this.postid = postid;
+		this.title = title;
+		this.body = body;
+	}
+}
